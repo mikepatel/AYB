7b9d293971cb04206ea6a571b0e89a68cbc5f0e4
Michael Patel <michael.patel94@gmail.com>
2020-07-29 14:41:58 -0400

    Populated Train dir

--- train.py
+++ train.py
@@ -40,15 +40,15 @@ if __name__ == "__main__":
     print(f'Number of rows with Black=1: {len(df.loc[df["Black"] == 1])}')
 
     # use df to copy images to appropriate directories
-    #image_files = os.listdir(IMAGES_DIR)
     # populate Training directory
     for index, row in df.iterrows():
-        image_filepath = os.path.join(IMAGES_DIR, row["image_id"])
+        image_filename = row["image_id"]
+        image_filepath = os.path.join(IMAGES_DIR, image_filename)
         image = Image.open(image_filepath)
 
         if row["Black"] == 1:  # save in "Black"
-            image.save(os.path.join(TRAIN_DIR, "Black\\"+row["image_id"]))
+            image.save(os.path.join(TRAIN_DIR, "Black\\"+image_filename))
 
         else:  # save in "Not Black"
-            image.save(os.path.join(TRAIN_DIR, "Not Black\\"+row["image_id"]))
+            image.save(os.path.join(TRAIN_DIR, "Not Black\\"+image_filename))
 
